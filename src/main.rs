6dc42dc581aab9cef0f84d52166376305c75c53a
arlohb <arlo@urwick.co.uk>
2024-06-25 22:50:49 +0100

    Moved add_player to player.rs

--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,6 @@ pub use time_to_live::*;
 mod shooting;
 
 use bevy::prelude::*;
-use movement::{Movement, Velocity, WrapMovement};
-
-fn add_player(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn((
-        SpriteBundle {
-            transform: Transform::from_scale(Vec3::new(3., 3., 3.)),
-            // Not yet decided on outline or not
-            texture: asset_server.load("ship_outline.png"),
-            ..Default::default()
-        },
-        Velocity {
-            velocity: Vec3::ZERO,
-            drag: 1.5,
-        },
-        Movement { speed: 3200. },
-        WrapMovement,
-        Player,
-    ));
-}
 
 fn add_camera(mut commands: Commands) {
     commands.spawn((Camera2dBundle::default(),));
--- src/player.rs
+++ src/player.rs
@@ -1,4 +1,23 @@
+use crate::movement::{Movement, Velocity, WrapMovement};
 use bevy::prelude::*;
 
 #[derive(Component)]
 pub struct Player;
+
+pub fn add_player(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform::from_scale(Vec3::new(3., 3., 3.)),
+            // Not yet decided on outline or not
+            texture: asset_server.load("ship_outline.png"),
+            ..Default::default()
+        },
+        Velocity {
+            velocity: Vec3::ZERO,
+            drag: 1.5,
+        },
+        Movement { speed: 3200. },
+        WrapMovement,
+        Player,
+    ));
+}
